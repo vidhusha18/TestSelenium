95a8ed3afc527fcd09cfdb0c2051988a37324d9f
vidhusha <vidhushasivakumar18@gmail.com>
2024-07-30 16:47:20 +0530

    files added

---
+++ src/test/java/com/vidhu/asmnt/TestingAssementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vidhu.asmnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingAssementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
